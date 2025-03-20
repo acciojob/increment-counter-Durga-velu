5d08a94ffa67d5a003fc7c908e543f59c18e8391
Durga-velu <durgadevi134babu@gmail.com>
2025-03-21 00:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let counter's=0;
+function  add() {
+	let count=document.querySelector('.counter')
+	
+	counter=counter+Math.floor(count.value)
+	
+	count.innerHTML=counter
+}
+
